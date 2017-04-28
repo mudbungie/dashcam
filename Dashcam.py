496df9c5e5eed65f3fba937d34a7b5340f2186b7
mudbungie <mudbungie@mudbungie.net>
2017-04-28 03:14:47 -0700

    testing has to be moved to pi

--- Dashcam.py
+++ Dashcam.py
@@ -2,4 +2,18 @@
 
 # Daemon that watches the camera, and records things.
 
+import picamera
+import time
 
+def get_config():
+    # Open a JSON file
+
+# Return a configured camera.
+def init_cam():
+    cam = picamera.PiCamera()
+    cam.resolution = (1280, 720)
+    return cam
+
+def start_recording():
+    cam = init_cam()
+    
